635497ad5bd3ce3f0cb5a58c029545f2fe0e3537
Huangjx <zhandouxiaojiji@gmail.com>
2018-07-24 21:57:32 +0800

    agent和watchdog通用

--- service/socket/agent.lua
+++ service/socket/agent.lua
@@ -1,232 +1,52 @@
-local skynet = require "skynet.manager"
-local cluster = require "skynet.cluster"
+local skynet = require "skynet"
 local socket = require "skynet.socket"
-local packet = require "packet"
-local opcode = require "define.opcode"
-local errcode = require "define.errcode"
-local util = require "common.util"
-local prop = require "prop"
-local config = require "config"
-local db = require "common.db"
+local sproto = require "sproto"
 
-local trace = util.trace("agent")
+local player = ...
+player = require(player)
 
-local loginserver
-local gameserver
-local warserver
+local FD
+local WATCHDOG
+local GATE
 
-local client
-local gate
 local CMD = {}
-local NORET = "NORET"
-local session = 0
-local protobuf
-local player
+local client_fd
+
+local function send_package(pack)
+	local package = string.pack(">s2", pack)
+	socket.write(client_fd, package)
+end
 
 skynet.register_protocol {
 	name = "client",
 	id = skynet.PTYPE_CLIENT,
-	unpack = function (buffer, len)
-        return packet.unpack(buffer, len)
-    end,
-    dispatch = function (_, _, op, session, buffer, len)
-        local opname = opcode.toname(op)
-        local modulename = opcode.tomodule(op)
-        local simplename = opcode.tosimplename(op)
-        if opcode.has_session(op) then
-            trace("recv package, 0x%x %s, session:%d", op, opname, session)
-        end
-
-        local data = protobuf.decode(opname, buffer, len)
-       
-        local ret = 0 -- 返回整数为错误码，table为返回客户端数据
-        if op >= 0xa000 and op <= 0xafff then
-            if not skynet.try(function()
-                ret = skynet.call(loginserver, "lua", "recv_package", skynet.self(), modulename,
-                    simplename, data)
-            end) then
-                ret = errcode.Traceback
-            end
-        end
-
-        if op >= 0x1000 and op <= 0x4fff and player then
-            if not skynet.try(function()
-                assert(player, "player nil")
-                assert(player[modulename], string.format("module nil [%s.%s]", modulename, simplename))
-                assert(player[modulename][simplename], string.format("handle nil [%s.%s]", modulename, simplename))
-                ret = player[modulename][simplename](player[modulename], data) or 0
-            end) then
-                ret = errcode.Traceback
-            end
-        end
-
-        if op >= 0x5000 and op <= 0x9fff then
-            trace("send to warserver")
-            if not skynet.try(function()
-                ret = skynet.call(warserver, "lua", "recv_package", skynet.self(), 
-                player.player_id, op, data) or 0
-            end) then
-                ret = errcode.Traceback
-            else 
-                --return
-            end
-        end
-        assert(ret, string.format("no respone, opname %s", opname))
-        if type(ret) == "table" then
-            ret.err = ret.err or 0
-        else
-            ret = {err = ret} 
-        end
-        CMD.send_package(op, ret)
-    end
+	unpack = function (msg, sz)
+		return host:dispatch(msg, sz)
+	end,
+	dispatch = function (fd, _, type, ...)
+		assert(fd == FD)	-- You can use fd to reply message
+		skynet.ignoreret()	-- session is fd, don't call skynet.ret
+		skynet.trace()
+	end
 }
 
-function CMD.get_session()
-    return session
-end
+function CMD.start(conf)
+	FD = conf.fd
+	GATE = conf.gate
+    WATCHDOG = conf.WATCHDOG
 
-function CMD.change_socket(conf)
-    socket.close(player.conf.client)
-    player.conf = conf
-    player.is_online = true
-    client = conf.client
-    gate = conf.gate
-    skynet.log("change_socket, %d", conf.client)
-    skynet.call(gate, "lua", "forward", client)
-    player:send_package(opcode.login.s2c_login, {err = 0, reconnected = true})
-end
-
-function CMD.is_online()
-    if player and player.is_online then
-        return true
-    else
-        return false
-    end
-end
-
-function CMD.init_by_gameserver(conf)
-    trace("agent start_game")
-	client = conf.client
-	gate = conf.gate
-    
-    local protobuf_env = skynet.call(conf.server, "lua", "get_protobuf_env")
-    assert(type(protobuf_env) == "userdata")
-    assert(not package.loaded["protobuf"])
-    debug.getregistry().PROTOBUF_ENV = protobuf_env
-    protobuf = require "protobuf"  
-
-    player = require "gameserver.player"
-    player.conf = conf
-    player:load_module()
-    player.gameserver:set(conf.server)
-    player.send_package = function(player, op, data)
-        local ret = skynet.try(function()
-            CMD.send_package(op, data)
-        end)
-        if not ret then
-            trace(util.dump(data))
-            CMD.send_package(op, {err = errcode.Traceback})
-        end
-    end
-    
-    skynet.call(gate, "lua", "forward", client)
-end
-
-function CMD.init_by_loginserver(conf)
-    trace("agent start_game")
-	client = conf.client
-	gate = conf.gate
-    
-    local protobuf_env = skynet.call(conf.server, "lua", "get_protobuf_env")
-    assert(type(protobuf_env) == "userdata")
-    assert(not package.loaded["protobuf"])
-    debug.getregistry().PROTOBUF_ENV = protobuf_env
-    protobuf = require "protobuf"  
-        
-    skynet.call(gate, "lua", "forward", client)
-end
-
-function CMD.bind_loginserver(addr)
-    loginserver = addr
-end
-
-function CMD.bind_gameserver(addr)
-    gameserver = addr
-end
-
-function CMD.bind_warserver(clustername, warname)
-    warserver = cluster.proxy(clustername, warname)
-    trace("bind_warserver %s %s", warname, warserver)
-end
-
-
-function CMD.send_package(op, tbl)
-    tbl.err = tbl.err or 0
-
-    assert(op, "op nil")
-    assert(opcode.toname(op), string.format("op 0x%x not exist!", op))
-    assert(tbl, "send data nil")
-    
-    if opcode.has_session(op) then
-        session = session + 1
-    end
-
-    skynet.log("send_package 0x%x %s errcode:%s", op, opcode.toname(op), tbl.err)
-
-    local data, len
-    protobuf.encode(opcode.toname(op), tbl, function(buffer, bufferlen)
-        data, len = packet.pack(op, session, buffer, bufferlen)
-    end)
-    
-    socket.write(client, data, len)
-    return NORET
+	skynet.call(GATE, "lua", "forward", fd)
 end
 
 function CMD.disconnect()
-    trace("disconnect")
-    if player and player.is_online then
-        player:offline()
-    end
-    skynet.exit()
-    return NORET
-end
-
-function CMD.kick()
-    skynet.fork(function()
-        skynet.exit()
-    end)
-end
-
-function CMD.call_player(modulename, func, ...)
-    trace("call_player %s, %s", modulename, func)
-    if type(player[modulename]) == "function" then
-        return player[modulename](player, ...)
-    end
-    return player[modulename][func](player[modulename], ...)
+	-- todo: do something before exit
+	skynet.exit()
 end
 
 skynet.start(function()
-	skynet.dispatch("lua", function(_,_, command1, command2, ...)
-        local ret = NORET
-		local f = CMD[command1]
-        if f then
-    		ret = f(command2, ...)
-        elseif player[command1] then
-            local module = player[command1]
-            if type(module) == "function" then
-                ret = module(player, command2, ...)
-            else
-                ret = module[command2](module, ...)
-            end
-        end
-        if ret ~= NORET then
-            skynet.ret(skynet.pack(ret))
-        end
+	skynet.dispatch("lua", function(_,_, command, ...)
+		skynet.trace()
+		local f = CMD[command]
+		skynet.ret(skynet.pack(f(...)))
 	end)
-
-    prop:init()
-
-    --loginserver = cluster.proxy("loginserver", ".loginserver")
-    
 end)
-
--- service/socket/watchdog.lua
+++ service/socket/watchdog.lua
@@ -1,296 +1,92 @@
-local skynet = require "skynet.manager"
-local cluster = require "skynet.cluster"
-local protobuf_c = require "protobuf.c"
-local config = require "config"
-local errcode = require "define.errcode"
-local db = require "common.db"
-local util = require "common.util"
-local os_utility = require "common.os_utility"
-local date_utility = require "common.date_utility"
-local prop = require "prop"
-local gmcmd = require "gameserver.gm.gmcmd"
+local skynet = require "skynet"
 
-local gate
-local agent2fd = {}
-local server_id = 0
-local server_name = ""
-local id2agent = {}       
-local id2player = {}
-local agent2player = {}
-local account2agent = {}
+local server, player = ...
+assert(server) -- 服务器逻辑(xxx.xxxserver)
+assert(player) -- 玩家逻辑(xxx.xxxplayer)
 
-local NORET = "NORET"
-local trace = util.trace("gameserver")
+local server = require(server)
 
-local function init_protobuf()
-    debug.getregistry().PROTOBUF_ENV = protobuf_c._env_new()
-    local protobuf = require "protobuf"
-    protobuf.register_file(config.workspace .. "/script/define/proto/package.pb")
-end
-
-local gameserver = {
-}
-
-function gameserver:get_protobuf_env()
-    return debug.getregistry().PROTOBUF_ENV
-end
-
-function gameserver:reload()
-    for player_id,agent in pairs(account2agent) do
-        skynet.call(agent, "lua", "reload_player")
-    end
-end
-
-function gameserver:send_to_world(op, data)
-    for player_id,agent in pairs(account2agent) do
-        local clone_data = {}
-        for k,v in pairs(data) do
-            clone_data[k] = v
-        end
-        skynet.send(agent, "lua", "send_package", op, clone_data)
-    end
-    return 0
-end
-
-function gameserver:send_to_player(op, data, player_id)
-    local agent = id2agent[player_id]
-    if not agent then
-        return errcode.PlayerOffline
-    end
-    skynet.send(agent, "lua", "send_package", op, data)
-    return 0
-end
-
-function gameserver:broadcast(...)
-    local args = table.pack(...)
-    skynet.try(function()
-        for _, agent in pairs(account2agent) do
-            skynet.send(agent, "lua", table.unpack(args))
-        end
-    end)
-end
-
-function gameserver:call_player(player_id, ...)
-    local agent = id2agent[player_id]
-    if not agent or not skynet.call(agent, "lua", "is_online") then
-        return errcode.PlayerOffline
-    end
-    return skynet.call(agent, "lua", "call_player", ...)
-end
-
-function gameserver:agent_create(player_id, account, agent)
-
-end
-
-function gameserver:player_online(account, player_id, agent)
-    account2agent[account] = agent
-    id2agent[player_id] = agent
-end
-
-function gameserver:player_offline(account, player_id)
-    account2agent[account] = nil
-    id2agent[player_id] = nil
-end
-
-function gameserver:get_agent_by_player_id(player_id)
-    return id2agent[player_id]
-end
-
-function gameserver:get_agent_by_account(account)
-    return account2agent[account]
-end
-
-function gameserver:server_id()
-    return server_id
-end
-
-function gameserver:new_open()
-end
-
-function gameserver:get_online_count()
-    local count = 0
-    for k, v in pairs(agent2fd) do
-        count = count + 1
-    end
-    return count
-end
-
-function gameserver:get_detail_data()
-    local data = {
-        server_name = server_name,
-        online_count = self:get_online_count(),
-    } 
-    return data
-end
-
-local log_file_name
-local log_file
-local function _log(format, ...)
-    local str = "["..os.date("%H:%M:%S", os.time()).."]"..string.format(format, ...)
-    local new_file_name = config.workspace.."/log/gameserver/"..server_id
-        .."/"..os.date("%Y%m%d", os.time())..".log"
-    if log_file_name ~= new_file_name then
-        if log_file then
-            log_file:close()
-        end
-        os_utility.create_folder(config.workspace.."/log/gameserver/"..server_id)
-        log_file = io.open(new_file_name, "a+")
-        log_file_name = new_file_name
-    end    
-    log_file:write(str.."\n")
-    log_file:flush()
-
-    skynet.error(str)
-end
-
+local CMD = {}
 local SOCKET = {}
-function SOCKET.open(fd, addr)
-    trace("New client from : " .. addr)
-    agent2fd[fd] = skynet.newservice("agent")
-    skynet.call(agent2fd[fd], "lua", "init_by_gameserver", {gate = gate, client = fd, server = skynet.self()})
-    skynet.call(agent2fd[fd], "lua", "bind_gameserver", skynet.self())
+local gate
+local agent = {}
+local fd2agent = {} -- socket对应的agent
+local acc2agent = {} -- 每个账号对应的agent
+local free_list = {} -- 空闲的agent list
+
+local table_insert = table.insert
+local table_remove = table.remove
+
+local function pop_free_agent()
+    local agent = free_list[#free_list]
+    if agent == 0 then
+        return skynet.newservice("agent", player)
+    end
+    free_list[#free_list] = nil
+    return agent
 end
 
-local function close_agent(fd)
-    local a = agent2fd[fd]
-    agent2fd[fd] = nil
-    if a then
-        skynet.call(gate, "lua", "kick", fd)
-        skynet.send(a, "lua", "disconnect")
-    end
+local function close_socket(fd)
+ 	skynet.call(gate, "lua", "kick", fd)
+    fd2agent[fd] = nil   
+end
+
+function SOCKET.open(fd, addr)
+	skynet.error("New client from : " .. addr)
+	fd2agent[fd] = pop_free_agent()
+	skynet.call(fd2agent[fd], "lua", "start", { gate = gate, client = fd, watchdog = skynet.self() })
 end
 
 function SOCKET.close(fd)
-    trace("socket close",fd)
-    close_agent(fd)
+	print("socket close",fd)
+    close_socket(fd)
 end
 
 function SOCKET.error(fd, msg)
-    trace("socket error",fd, msg)
-    close_agent(fd)
+	print("socket error",fd, msg)
+    close_socket(fd)
 end
 
 function SOCKET.warning(fd, size)
-    trace("socket warning", fd, size)
+	-- size K bytes havn't send out in fd
+	print("socket warning", fd, size)
 end
 
 function SOCKET.data(fd, msg)
-    trace("socket data")
 end
 
-function gameserver:start(conf)
-    prop:init()
-    skynet.call(gate, "lua", "open", conf)
-    server_id = conf.id
-    server_name = conf.name
-   
-    local data = db.find_one_with_default("gameserver", 
-        {server_id = server_id}, {server_id = server_id})
-
-    skynet.log = _log
-    skynet.log("gameserver start !!!!")
-
-    db.disconnect()
-end
-
-function gameserver:stop()
-    skynet.log("gameserver %d will be stop", server_id)
-    skynet.try(function()
-        gameserver:save()
-        skynet.abort()
-        return NORET
-    end)
-end
-
-function gameserver:abort()
-    skynet.log("gameserver %d will be stop", server_id)
-    gameserver:save()
-    skynet.timeout(0, function()
-        skynet.abort() 
-    end)
-end
-
-function gameserver:save()
-    for _, agent in pairs(account2agent) do
-        skynet.try(function()
-            skynet.call(agent, "lua", "call_player", "save")
-        end)
+function CMD.start(conf, preload)
+    preload = preload or 10     -- 预加载agent数量
+	skynet.call(gate, "lua", "open" , conf)
+    for i = 1, preload do
+        local agent = skynet.newservice("socket/agent", player)
+        table_insert(free_list, agent)
     end
-
-    skynet.log("gameserver %d saved", server_id)
 end
 
-function gameserver:close(fd)
-    close_agent(fd)
+-- 上线后agent绑定acc，下线缓存一段时间
+function CMD.bind_acc(agent, acc)
+    acc2agent[acc] = agent
 end
 
-function gameserver:execute_gm(cmd, ...)
-    return gmcmd[cmd](skynet.self(), ...) 
-end
-
-function gameserver:point_logic()
-    for _, agent in pairs(account2agent) do
-        skynet.send(agent, "lua", "point_logic")
-    end
-    
-    local hour = date_utility.hour()
-    skynet.log("gameserver point_logic, %d", hour)
-    local func = self["point_logic_"..hour]
-    if func then
-        func(self)
-    end
-
-    self:save()
-   
-    return NORET
-end
-
-function gameserver:point_logic_23()
-end
-
-function gameserver:init_cluster()
-    local clustername = skynet.getenv("clustername")
-    skynet.register("."..clustername)         
-    cluster.open(clustername)
-
-    local server_idx = tonumber(string.match(clustername, "%d+"))
-    local conf = cluster.call("master", ".server_manager", "on_gameserver_start", server_idx, clustername)
-    self:start(conf)
+-- 下线一段时间后调用
+function CMD.free_agent(acc, agent)
+    table_insert(free_list, agent) 
+    acc2agent[acc] = nil
 end
 
 skynet.start(function()
-    skynet.dispatch("lua", function(session, source, cmd, subcmd, ...)
-        --trace("%s, %s", cmd, subcmd)
-        local function ret_value(noret, ...)
-            if noret ~= NORET then
-                skynet.log("%s %s", cmd, noret)
-                skynet.ret(skynet.pack(noret, ...))
-            end
-        end
+	skynet.dispatch("lua", function(session, source, cmd, subcmd, ...)
+		if cmd == "socket" then
+			local f = SOCKET[subcmd]
+			f(...)
+			-- socket api don't need return
+		else
+			local f = assert(CMD[cmd])
+			skynet.ret(skynet.pack(f(subcmd, ...)))
+		end
+	end)
 
-        local ret = NORET
-        if cmd == "socket" then
-            local f = SOCKET[subcmd]
-            f(...)
-            return
-        else
-            local f = assert(gameserver[cmd], cmd)
-            if type(f) == "function" then
-                ret_value(f(gameserver, subcmd, ...))
-            else
-                ret_value(f[subcmd](f, ...))
-            end
-        end
-    end)
-
-    init_protobuf()
-
-    gate = skynet.newservice("gate")
-
-    loginserver = cluster.proxy("loginserver", ".loginserver")
-
-    gameserver:init_cluster()
+	gate = skynet.newservice("gate")
+    
 end)
-
-
-
