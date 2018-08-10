4edb313d433b402ec945a9838a8c9e20a61067ed
huangjx <zhandouxiaojiji@gmail.com>
2018-08-10 16:14:02 +0800

    房间消息同步

--- lualib/battle/battle_t.lua
+++ lualib/battle/battle_t.lua
@@ -1,6 +1,7 @@
 local skynet = require "skynet"
 local class = require "class"
 local sname = require "sname"
+local opcode = require "def.opcode"
 local def = require "def"
 
 local M = class("battle_t")
@@ -9,10 +10,15 @@ function M:ctor(player)
     self.mode = def.BattleMode.NORMAL
     self.room_id = nil
     self.room_agent = nil
+    self.enemy = nil
 end
 
-function M:call_room(...)
-    return skynet.call(self.room_agent, "lua", self.room_id, ...)
+function M:call_room(cmd, ...)
+    return skynet.call(self.room_agent, "lua", self.room_id, cmd, self.player.uid, ...)
+end
+
+function M:send(op, data)
+    self.player.net:send(op, data) 
 end
 
 function M:init_by_data(data)
@@ -32,13 +38,41 @@ function M:is_robot()
     return self.enemyid == 0
 end
 
-function M:start(enemyid)
+function M:create_room(enemyid)
     self.enemyid = enemyid
     local room_id, agent = skynet.call(sname.ROOMCENTER, "lua", "create_room", self.player.uid, enemyid)
     self.room_id = room_id
     self.room_agent = agent
-    self:call_room("join", self.player.uid, skynet.self())
+    self:call_room("join", skynet.self(), {
+        uid = self.player.uid,
+        gender = self.player.user.gender,
+        avatar = self.player.user.avatar,
+        name = self.player.user.name,
+    })
 end
 
+function M:on_join(uid, enemy)
+    self.enemy = enemy
+    self:send(opcode.battle.s2c_player_join, {
+        player = {
+            uid = enemy.uid,
+            gender = enemy.gender,
+            avatar = enemy.avatar,
+            name = enemy.name,
+        }
+    })
+end
+
+function M:ready()
+    self:call_room("ready")
+end
+
+function M:sync(score)
+    self:call_room("sync", score)
+end
+
+function M:giveup()
+    self:call_room("giveup")
+end
 
 return M
--- lualib/battle/player_battle_t.lua
+++ lualib/battle/player_battle_t.lua
@@ -1,6 +1,7 @@
 local skynet = require "skynet"
 local class = require "class"
 local sname = require "sname"
+local opcode = require "def.opcode"
 local def = require "def"
 
 local M = class("player_battle_t")
@@ -10,7 +11,7 @@ function M:ctor(player)
        local battle_class = require(def.BattleClass[t])
        self[t] = battle_class.new(player)
     end
-    self.cur_battle = nil
+    self.battle = nil
 end
 
 function M:init_by_data(data)
@@ -22,14 +23,20 @@ function M:base_data()
     }
 end
 
+function M:room_command(cmd, ...)
+    assert(self.battle)
+    assert(self.battle[cmd], cmd)
+    return self.battle[cmd](self.battle, ...)
+end
+
 function M:match(mode)
     skynet.call(sname.MATCHCENTER, "lua", "match", mode, 
         self.player.uid, 1, skynet.self()) 
 end
 
 function M:matched(mode, targetid)
-    self.cur_battle = assert(self[mode])
-    self.cur_battle:start(targetid)
+    self.battle = assert(self[mode])
+    self.battle:create_room(targetid)
 end
 
 function M:create_room()
@@ -37,11 +44,24 @@ function M:create_room()
 end
 
 function M:ready()
+    if not self.battle then
+        return errocode.NotInBattle
+    end
+    self.battle:ready()
+end
 
+function M:sync(score)
+    if not self.battle then
+        return errocode.NotInBattle
+    end
+    self.battle:sync(score)
 end
 
 function M:giveup()
-
+    if not self.battle then
+        return errocode.NotInBattle
+    end
+    self.battle:giveup()
 end
 
 -- network
@@ -54,11 +74,11 @@ function M:c2s_create_room(data)
 end
 
 function M:c2s_ready(data)
-
+    return self:ready()
 end
 
 function M:c2s_sync(data)
-
+    return self:sync(data.score)
 end
 
 function M:c2s_giveup(data)
